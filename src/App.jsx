71340f0a0147294be1cb2e89f7ef8571aa310c9b
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-03-26 13:19:44 -0700

    beginning CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
+import './App.css'
+
 
 
  const App = () => {
   const [puppies, setPuppies] = useState(puppyList);
+  const [currentPupId, setCurrentPupId] = useState(null)
+  const currentPup = puppies.find((pup) => pup.id === currentPupId)
+  console.log(currentPup)
   return (
     <>
      {
-      puppies.map((puppy) => {return <p key={puppy.id}>{puppy.name}</p>})
+      puppies.map((puppy) => {
+        return <p onClick={() => {setCurrentPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+      })
      }
+      <div>{currentPupId && (
+        <div>
+          <h2>{currentPup.name}</h2>
+          <ul>
+            <li>Age: {currentPup.age}</li>
+            <li>Email: {currentPup.email}</li>
+          </ul>
+        </div>  
+      )}
+      </div>
     </>
   )
 }
